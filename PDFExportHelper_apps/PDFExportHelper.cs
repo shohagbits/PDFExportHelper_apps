ddd5a58507a5350b2c6c1b562dd582953848adb5
shohag.mia <shohag.mia@bracits.com>
2024-02-14 16:10:02 +0600

    Add project files.

---
+++ PDFExportHelper_apps/PDFExportHelper.cs
@@ -0,0 +1,70 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDFExportHelper_apps
+{
+    public static class PDFExportHelper
+    {
+        public static void ExportToPdf(DataTable dt, string filePath_fileName)
+        {
+            iTextSharp.text.Document document = new iTextSharp.text.Document();
+            PdfWriter writer = PdfWriter.GetInstance(document, new FileStream(filePath_fileName, FileMode.Create));
+            document.Open();
+            iTextSharp.text.Font font5 = iTextSharp.text.FontFactory.GetFont(FontFactory.HELVETICA, 12);
+
+            PdfPTable table = new PdfPTable(dt.Columns.Count);
+            PdfPRow row = null;
+            float[] widths = new float[dt.Columns.Count];
+            for (int i = 0; i < dt.Columns.Count; i++)
+                widths[i] = 4f;
+
+            table.SetWidths(widths);
+
+            table.WidthPercentage = 100;
+            int iCol = 0;
+            string colname = "";
+            PdfPCell cell = new PdfPCell(new Phrase("Products"));
+
+            cell.Colspan = dt.Columns.Count;
+            AddCellToHeader(table, "Statement of Financial Position");
+            AddCellToHeader(table, "December 31, 2018");
+
+
+            foreach (DataColumn c in dt.Columns)
+            {
+                table.AddCell(new Phrase(c.ColumnName, font5));
+            }
+            foreach (DataRow r in dt.Rows)
+            {
+                if (dt.Rows.Count > 0)
+                {
+                    for (int h = 0; h < dt.Columns.Count; h++)
+                    {
+                        table.AddCell(new Phrase(r[h].ToString(), font5));
+                    }
+                }
+            }
+            document.Add(table);
+            document.Close();
+        }
+        private static void AddCellToHeader(PdfPTable tableLayout, string cellText)
+        {
+            tableLayout.AddCell(new PdfPCell(new Phrase(cellText, new Font(Font.NORMAL, 13, 1, new iTextSharp.text.BaseColor(153, 51, 0)))) { Colspan = 5, Border = 0, PaddingBottom = 5, HorizontalAlignment = Element.ALIGN_CENTER });
+        }
+        private static void AddCellToTableHeader(PdfPTable tableLayout, string cellText)
+        {
+            tableLayout.AddCell(new PdfPCell(new Phrase(cellText, new Font(Font.NORMAL, 8, 1, iTextSharp.text.BaseColor.WHITE))) { HorizontalAlignment = Element.ALIGN_CENTER, Padding = 5, BackgroundColor = new iTextSharp.text.BaseColor(0, 51, 102) });
+        }
+        private static void AddCellToBody(PdfPTable tableLayout, string cellText)
+        {
+            tableLayout.AddCell(new PdfPCell(new Phrase(cellText, new Font(Font.NORMAL, 8, 1, iTextSharp.text.BaseColor.BLACK))) { HorizontalAlignment = Element.ALIGN_CENTER, Padding = 5, BackgroundColor = iTextSharp.text.BaseColor.WHITE });
+        }
+    }
+}
---
+++ PDFExportHelper_apps/Program.cs
@@ -0,0 +1,36 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Data;
+
+namespace PDFExportHelper_apps
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var newDataTable = new DataTable();
+
+            var headerColumns = new List<string>() { "SL", "MobileNo", "City" };
+            foreach ( var item in headerColumns)
+            {
+                var dataColumn = new DataColumn(item);
+                newDataTable.Columns.Add(dataColumn);
+            }
+
+            DataRow toInsert = newDataTable.NewRow();
+            int sl = 1;
+            foreach (var column in headerColumns)
+            {   
+                toInsert[column] = $"Test Value {sl}";
+            }
+            foreach (var column in headerColumns)
+            {
+                toInsert[column] = $"Test Value {sl}";
+            }
+            newDataTable.Rows.Add(toInsert);
+
+            PDFExportHelper.ExportToPdf(newDataTable, "D:\\BracBank\\FeatureDevelopment\\21_40462_Backend_Report_Processing\\pdfTest.pdf");
+
+            Console.ReadLine();
+        }
+    }
+}
